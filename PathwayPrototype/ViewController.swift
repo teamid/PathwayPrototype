c73b8df7e12a415cd963276a733574ea6e39866a
David <teamid@gmail.com>
2015-11-12 13:20:58 -0800

    Initial Commit

---
+++ PathwayPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PathwayPrototype
+//
+//  Created by David Smith on 11/12/15.
+//  Copyright © 2015 David Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+      override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+      }
+
+      override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+      }
+
+
+}
+
